c4994f41455babc19872ea8e518fdc5897e8cdba
Techbeast <animasauntemidayoprosper@gmail.com>
2023-10-22 19:30:55 +0100

    update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import ejs from 'ejs';
 import mongoose from 'mongoose';
-import encrypt from 'mongoose-encryption'
+import md5 from 'md5';
 import { Console } from 'console';
 
 const port = 1234;
@@ -14,7 +14,9 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true })
+   const connectionString = process.env.MONGODB_CONNECT_URI;
+
+   mongoose.connect(connectionString, { useNewUrlParser: true })
     .then(() => {
         console.log("Connected to the database");
     })
@@ -25,7 +27,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true })
         email:String,
         password:String,
     });
-    userSchema.plugin(encrypt, { secret:process.env.SECRET, encryptedFields:['password']});
+  
 
     const User = new mongoose.model("User",userSchema)
 
@@ -45,7 +47,7 @@ app.post("/register", async(req,res)=>{
 
     const newUser = new User({
         email:req.body.email,
-        password:req.body.password
+        password:md5(req.body.password)
     });
 
     newUser.save()
@@ -60,7 +62,7 @@ res.render("secrets.ejs");
 });
 app.post("/login", async(req,res)=>{
     const email = req.body.email;
-    const password=req.body.password
+    const password=md5(req.body.password);
   await User.findOne({email:email})
   .then(loginUser =>{
         if(loginUser.password === password){
